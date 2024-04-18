2d11c544e2725c12248e7e31686de95a69fd8766
Bhavana <nbhavana2001@gmail.com>
2024-04-18 11:11:08 +0530

    BankApplication

---
+++ BankApplication/src/Bank.java
@@ -0,0 +1,7 @@
+public class Bank {
+    public static void main(String[] args){
+        System.out.println("$$$ WELCOME TO Bhavana BANK $$$");
+        new BankUI();
+        System.out.println("THANK YOU FOR VISITING...");
+    }
+}
